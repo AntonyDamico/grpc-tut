058e4ffc2c300f312c1d17b280b7cb0452a0bbb2
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-23 20:16:41 +0200

    Creating client

---
+++ client.js
@@ -0,0 +1,16 @@
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader')
+
+const text = process.argv[2];
+const packageDef = protoLoader.loadSync('todo.proto', {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const todoPackage = grpcObject.todoPackage;
+
+const client = new todoPackage.Todo(
+  'localhost:40000',
+  grpc.credentials.createInsecure(),
+);
+
+client.createTodo({ text }, (err, response) => {
+  console.log(err, response)
+});
